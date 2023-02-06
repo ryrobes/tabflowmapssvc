d2f66e248a7a2974fdbff97a6a1a4b09b88a4f1f
Ryan Robitaille <ryan.robitaille@gmail.com>
2023-02-06 15:54:19 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject tabflowmapssvc "0.1.0-SNAPSHOT"
+(defproject tabflowmapssvc "0.1.0"
   :description "tabflowmapssvc - backend server component (processes the TWB into EDN for the front-end)"
   :url "https://github.com/ryrobes/tabflowmapssvc"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.analyzer "1.1.1"] ;; unused, but needed to stop ns clash warnings... :/
                  [mvxcvi/puget "1.3.2"] ;; https://github.com/greglook/puget
                  [com.wallbrew/clj-xml "1.7.2"]
                  [io.pedestal/pedestal.service "0.5.10"]
